a32267e71f12e4f31dd2c36353109d43c5bd533f
clara-cdp <clarianne.cdp@gmail.com>
2026-03-26 12:51:58 +0100

    initial api passport commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       // Passport::loadKeysFrom(__DIR__ . '/../secrets/oauth');
     }
 }
